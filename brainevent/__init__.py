274dca4e1a5f5f78ff1eefe5a181c437b37ee136
chaoming <adaduo@outlook.com>
2025-01-24 17:53:28 +0800

    fix setupy

--- brainevent/__init__.py
+++ brainevent/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
 from ._array import EventArray
 from ._csr import CSR, CSC
