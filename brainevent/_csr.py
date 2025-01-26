fix `CSR` spmv gradient computation

brainevent/_csr.py
@@ -15,18 +15,19 @@
 
 
 import operator
-from typing import Union
+from typing import Union, Sequence, Tuple
 
 import brainunit as u
 import jax
 import jax.numpy as jnp
+import numpy as np
 from brainunit.sparse._csr import _csr_to_coo, _csr_todense
 from jax.experimental.sparse import CSR
 from jax.experimental.sparse import JAXSparse
 
-from ._event import EventArray
 from ._csr_event_impl import _event_csr_matvec, _event_csr_matmat
 from ._csr_float_impl import _csr_matvec, _csr_matmat
+from ._event import EventArray
 
 __all__ = [
     'CSR',
@@ -47,7 +48,12 @@ class CSR(u.sparse.SparseMatrix):
     nse = property(lambda self: self.indices.size)
     dtype = property(lambda self: self.data.dtype)
 
-    def __init__(self, args, *, shape):
+    def __init__(
+        self,
+        args: Sequence[Union[jax.Array, np.ndarray, u.Quantity]],
+        *,
+        shape: Tuple[int, int]
+    ):
         self.data, self.indices, self.indptr = map(u.math.asarray, args)
         super().__init__(args, shape=shape)
 
@@ -314,7 +320,12 @@ class CSC(u.sparse.SparseMatrix):
     nse = property(lambda self: self.indices.size)
     dtype = property(lambda self: self.data.dtype)
 
-    def __init__(self, args, *, shape):
+    def __init__(
+        self,
+        args: Sequence[Union[jax.Array, np.ndarray, u.Quantity]],
+        *,
+        shape: Tuple[int, int]
+    ):
         self.data, self.indices, self.indptr = map(u.math.asarray, args)
         super().__init__(args, shape=shape)
 
@@ -579,4 +590,3 @@ class CSC(u.sparse.SparseMatrix):
         data, = children
         indices, indptr, shape = aux_data
         return CSC([data, indices, indptr], shape=shape)
-

brainevent/_csr_event_impl.py
@@ -125,6 +125,7 @@ def event_csrmv_cpu_kernel_generator(
             if vector_info.dtype == jnp.bool_:
                 @numba.njit(**numba_environ.setting)
                 def mv(weights, indices, indptr, v, _, posts):
+                    posts[:] = 0.
                     w = weights[0]
                     for i in range(v.shape[0]):
                         if v[i]:
@@ -134,6 +135,7 @@ def event_csrmv_cpu_kernel_generator(
             elif float_as_event:
                 @numba.njit(**numba_environ.setting)
                 def mv(weights, indices, indptr, v, _, posts):
+                    posts[:] = 0.
                     w = weights[0]
                     for i in range(v.shape[0]):
                         if v[i] != 0.:
@@ -143,6 +145,7 @@ def event_csrmv_cpu_kernel_generator(
             else:
                 @numba.njit(**numba_environ.setting)
                 def mv(weights, indices, indptr, v, _, posts):
+                    posts[:] = 0.
                     w = weights[0]
                     for i in range(v.shape[0]):
                         sp = v[i]
@@ -155,6 +158,7 @@ def event_csrmv_cpu_kernel_generator(
             if vector_info.dtype == jnp.bool_:
                 @numba.njit(**numba_environ.setting)
                 def mv(weights, indices, indptr, v, _, posts):
+                    posts[:] = 0.
                     w = weights[0]
                     for i in range(indptr.shape[0] - 1):
                         r = 0.
@@ -166,6 +170,7 @@ def event_csrmv_cpu_kernel_generator(
             elif float_as_event:
                 @numba.njit(**numba_environ.setting)
                 def mv(weights, indices, indptr, v, _, posts):
+                    posts[:] = 0.
                     w = weights[0]
                     for i in range(indptr.shape[0] - 1):
                         r = 0.
@@ -177,6 +182,7 @@ def event_csrmv_cpu_kernel_generator(
             else:
                 @numba.njit(**numba_environ.setting)
                 def mv(weights, indices, indptr, v, _, posts):
+                    posts[:] = 0.
                     w = weights[0]
                     for i in range(indptr.shape[0] - 1):
                         r = 0.
@@ -482,6 +488,7 @@ def event_csrmv_jvp_v(
     indices,
     indptr,
     v,
+    _,
     *,
     shape,
     transpose,
@@ -505,6 +512,7 @@ def event_csrmv_jvp_weights(
     indices,
     indptr,
     v,
+    _,
     *,
     shape,
     transpose,
@@ -528,6 +536,7 @@ def event_csrmv_transpose_rule(
     indices,
     indptr,
     events,
+    _,
     *,
     shape,
     float_as_event,
@@ -542,22 +551,25 @@ def event_csrmv_transpose_rule(
     if ad.is_undefined_primal(indices) or ad.is_undefined_primal(indptr):
         raise ValueError("Cannot transpose with respect to sparse indices.")
     if ad.is_undefined_primal(events):
-        ct_events = _csr_matvec(
-            data,
-            indices,
-            indptr,
-            ct,
-            shape=shape,
-            transpose=not transpose
-        )[0]
-        return data, indices, indptr, (ad.Zero(events) if type(ct) is ad.Zero else ct_events)
+        if type(ct) is ad.Zero:
+            ct_events = ad.Zero(events)
+        else:
+            ct_events = _csr_matvec(
+                data,
+                indices,
+                indptr,
+                ct,
+                shape=shape,
+                transpose=not transpose
+            )
+        return data, indices, indptr, ct_events, _
     else:
-        if type(ct[0]) is ad.Zero:
+        if type(ct) is ad.Zero:
             ct_values = ad.Zero(data)
         else:
             if data.aval.shape[0] == 1:  # scalar
                 ct_values = event_csrmv_p_call(
-                    jnp.ones(1, dtype=data.dtype),
+                    jnp.ones(1, dtype=data.aval.dtype),
                     indices,
                     indptr,
                     events,
@@ -565,11 +577,11 @@ def event_csrmv_transpose_rule(
                     transpose=transpose,
                     float_as_event=float_as_event,
                 )[0]
-                ct_values = jnp.inner(ct, ct_values)
+                ct_values = jnp.inner(ct, ct_values).reshape(*data.aval.shape)
             else:  # heterogeneous values
                 row, col = _csr_to_coo(indices, indptr)
                 ct_values = events[row] * ct[col] if transpose else events[col] * ct[row]
-        return ct_values, indices, indptr, events
+        return ct_values, indices, indptr, events, _
 
 
 def event_csrmv_batching(args, axes, **kwargs):
@@ -1057,7 +1069,18 @@ def event_csrmm_gpu_kernel_generator(
     return mm
 
 
-def csrmm_jvp_left(data_dot, data, indices, indptr, B, *, shape, transpose, **kwargs):
+def csrmm_jvp_left(
+    data_dot,
+    data,
+    indices,
+    indptr,
+    B,
+    _,
+    *,
+    shape,
+    transpose,
+    **kwargs
+):
     return [
         _csr_matmat(
             data_dot,
@@ -1070,7 +1093,18 @@ def csrmm_jvp_left(data_dot, data, indices, indptr, B, *, shape, transpose, **kw
     ]
 
 
-def csrmm_jvp_right(B_dot, data, indices, indptr, B, *, shape, transpose, **kwargs):
+def csrmm_jvp_right(
+    B_dot,
+    data,
+    indices,
+    indptr,
+    B,
+    _,
+    *,
+    shape,
+    transpose,
+    **kwargs
+):
     return [
         _csr_matmat(
             data,
@@ -1083,18 +1117,29 @@ def csrmm_jvp_right(B_dot, data, indices, indptr, B, *, shape, transpose, **kwar
     ]
 
 
-def csrmm_transpose_rule(ct, data, indices, indptr, B, *, shape, transpose, **kwargs):
+def csrmm_transpose_rule(
+    ct,
+    data,
+    indices,
+    indptr,
+    B,
+    _,
+    *,
+    shape,
+    transpose,
+    **kwargs
+):
     assert not ad.is_undefined_primal(indices)
     assert not ad.is_undefined_primal(indptr)
 
     if ad.is_undefined_primal(B):
         dB = _csr_matmat(data, indices, indptr, ct, shape=shape, transpose=not transpose)
-        return data, indices, indptr, dB
+        return data, indices, indptr, dB, _
     else:
         B = jnp.asarray(B)
         row, col = _csr_to_coo(indices, indptr)
         d_data = (ct[row] * B[col]).sum(axis=1)
-        return d_data, indices, indptr, B
+        return d_data, indices, indptr, B, _
 
 
 def event_csrmm_batching(args, axes, **kwargs):

brainevent/_csr_event_impl_test.py
@@ -17,12 +17,12 @@
 
 import unittest
 
+import brainstate as bst
 import brainunit as u
 import jax
 import jax.numpy as jnp
 
 import brainevent
-import brainstate as bst
 from brainevent._csr_test_util import _get_csr, vector_csr, matrix_csr, csr_vector, csr_matrix
 
 
@@ -108,7 +108,7 @@ class TestVectorCSR(unittest.TestCase):
             csr = brainevent.CSR([data, indices, indptr], shape=(m, n))
             y = brainevent.EventArray(x) @ csr
             y2 = vector_csr(x, csr.data, indices, indptr, [m, n])
-            self.assertTrue(jnp.allclose(y, y2, rtol=1e-3, atol=1e-3))
+            self.assertTrue(jnp.allclose(y, y2, rtol=1e-5, atol=1e-5))
 
     def test_vector_csr_vmap_vector(self):
         n_batch, m, n = 10, 20, 40
@@ -121,6 +121,102 @@ class TestVectorCSR(unittest.TestCase):
             y = jax.vmap(lambda x: brainevent.EventArray(x) @ csr)(xs)
             y2 = jax.vmap(lambda x: vector_csr(x, csr.data, indices, indptr, [m, n]))(xs)
             self.assertTrue(jnp.allclose(y, y2, rtol=1e-3, atol=1e-3))
+
+    def test_csr_vector(self):
+        m, n = 20, 40
+        v = bst.random.rand(n) < 0.1
+        indptr, indices = _get_csr(m, n, 0.2)
+
+        for homo_w in [True, False]:
+            data = 1.5 if homo_w else bst.init.Normal()(indices.shape)
+            csr = brainevent.CSR([data, indices, indptr], shape=(m, n))
+            y = csr @ brainevent.EventArray(v)
+            y2 = csr_vector(v, csr.data, indices, indptr, [m, n])
+            self.assertTrue(jnp.allclose(y, y2, rtol=1e-5, atol=1e-5))
+
+    def _test_vjp(self, homo_w, replace, transpose):
+        n_in = 20
+        n_out = 30
+        shape = [n_in, n_out]
+        x = bst.random.rand(n_in) if transpose else bst.random.rand(n_out)
+        x = (x < 0.6).astype(float)
+
+        indptr, indices = _get_csr(n_in, n_out, 0.2, replace=replace)
+        w = 1.5 if homo_w else bst.init.Normal()(indices.shape)
+        csr = brainevent.CSR((w, indices, indptr), shape=shape)
+
+        def f_brainevent(x, w):
+            if transpose:
+                r = brainevent.EventArray(x) @ csr.with_data(w)
+            else:
+                r = csr.with_data(w) @ brainevent.EventArray(x)
+            return r.sum()
+
+        r = jax.grad(f_brainevent, argnums=(0, 1))(x, w)
+
+        # -------------------
+        # TRUE gradients
+
+        def f_jax(x, w):
+            if transpose:
+                r = vector_csr(x, w, indices, indptr, shape=shape)
+            else:
+                r = csr_vector(x, w, indices, indptr, shape=shape)
+            return r.sum()
+
+        r2 = jax.grad(f_jax, argnums=(0, 1))(x, w)
+        self.assertTrue(jnp.allclose(r[0], r2[0], rtol=1e-3, atol=1e-3))
+        self.assertTrue(jnp.allclose(r[1], r2[1], rtol=1e-3, atol=1e-3))
+
+    def test_vjp(self):
+        for replace in [True, False]:
+            for transpose in [True, False]:
+                for homo_w in [True, False]:
+                    print(f'replace = {replace}, transpose = {transpose}, homo_w = {homo_w}')
+                    self._test_vjp(homo_w=homo_w, replace=replace, transpose=transpose)
+
+    def _test_jvp(self, homo_w, replace, transpose):
+        n_in = 20
+        n_out = 30
+        shape = [n_in, n_out]
+        x = bst.random.rand(n_in if transpose else n_out)
+        x = (x < 0.6).astype(float)
+
+        indptr, indices = _get_csr(n_in, n_out, 0.1, replace=replace)
+
+        w = 1.5 if homo_w else bst.init.Normal()(indices.shape)
+        csr = brainevent.CSR((w, indices, indptr), shape=shape)
+
+        def f_brainevent(x, w):
+            if transpose:
+                r = brainevent.EventArray(x) @ csr.with_data(w)
+            else:
+                r = csr.with_data(w) @ brainevent.EventArray(x)
+            return r
+
+        o1, r1 = jax.jvp(f_brainevent, (x, w), (jnp.ones_like(x), jnp.ones_like(w)))
+
+        # -------------------
+        # TRUE gradients
+
+        def f_jax(x, w):
+            if transpose:
+                r = vector_csr(x, w, indices, indptr,shape=shape)
+            else:
+                r = csr_vector(x, w, indices, indptr,shape=shape)
+            return r
+
+        o2, r2 = jax.jvp(f_jax, (x, w), (jnp.ones_like(x), jnp.ones_like(w)))
+        self.assertTrue(jnp.allclose(r1, r2, rtol=1e-3, atol=1e-3))
+        self.assertTrue(jnp.allclose(o1, o2, rtol=1e-3, atol=1e-3))
+
+    def test_jvp(self):
+        for replace in [True, False]:
+            for transpose in [True, False]:
+                for homo_w in [True, False]:
+                    print(f'replace = {replace}, transpose = {transpose}, homo_w = {homo_w}')
+                    self._test_jvp(homo_w=homo_w, replace=replace, transpose=transpose)
+
 
 
 class TestMatrixCSR(unittest.TestCase):
@@ -136,22 +232,6 @@ class TestMatrixCSR(unittest.TestCase):
             y2 = matrix_csr(x, csr.data, indices, indptr, [m, n])
             self.assertTrue(jnp.allclose(y, y2, rtol=1e-3, atol=1e-3))
 
-
-class TestCSRVector(unittest.TestCase):
-    def test_csr_vector(self):
-        m, n = 20, 40
-        v = bst.random.rand(n) < 0.1
-        indptr, indices = _get_csr(m, n, 0.1)
-
-        for homo_w in [True, False]:
-            data = 1.5 if homo_w else bst.init.Normal()(indices.shape)
-            csr = brainevent.CSR([data, indices, indptr], shape=(m, n))
-            y = csr @ brainevent.EventArray(v)
-            y2 = csr_vector(v, csr.data, indices, indptr, [m, n])
-            self.assertTrue(jnp.allclose(y, y2))
-
-
-class TestCSRMatrix(unittest.TestCase):
     def test_csr_matrix(self):
         m, n, k = 20, 40, 10
         matrix = bst.random.rand(n, k) < 0.1

brainevent/_csr_test_util.py
@@ -17,12 +17,19 @@
 
 import jax.numpy as jnp
 import numpy as np
+import brainstate as bst
 
 
-def _get_csr(n_pre, n_post, prob):
+def _get_csr(n_pre, n_post, prob, replace=True):
     n_conn = int(n_post * prob)
     indptr = np.arange(n_pre + 1) * n_conn
-    indices = np.random.randint(0, n_post, (n_pre * n_conn,))
+    if replace:
+        indices = np.random.randint(0, n_post, (n_pre * n_conn,))
+    else:
+        indices = bst.compile.for_loop(
+            lambda *args: bst.random.choice(n_post, n_conn, replace=False),
+            length=n_pre
+        ).flatten()
     return indptr, indices
 
 

brainevent/_event.py
@@ -47,6 +47,10 @@ def _as_jax_array_(obj):
     return obj.value if isinstance(obj, EventArray) else obj
 
 
+def _known_type(x):
+    return isinstance(x, (u.Quantity, jax.Array, np.ndarray))
+
+
 ArrayLike = Union[jax.Array, np.ndarray, u.Quantity]
 
 
@@ -327,14 +331,23 @@ class EventArray(object):
         return self
 
     def __matmul__(self, oc):
-        return self.value @ _as_jax_array_(oc)
+        if _known_type(oc):
+            return self.value @ _as_jax_array_(oc)
+        else:
+            return oc.__rmatmul__(self)
 
     def __rmatmul__(self, oc):
-        return _as_jax_array_(oc) @ self.value
+        if _known_type(oc):
+            return _as_jax_array_(oc) @ self.value
+        else:
+            return oc.__matmul__(self)
 
     def __imatmul__(self, oc):
         # a @= b
-        self.value = self.value @ _as_jax_array_(oc)
+        if _known_type(oc):
+            self.value = self.value @ _as_jax_array_(oc)
+        else:
+            self.value = oc.__rmatmul__(self)
         return self
 
     def __and__(self, oc):
@@ -510,7 +523,10 @@ class EventArray(object):
 
     def dot(self, b):
         """Dot product of two arrays."""
-        return self.value.dot(_as_jax_array_(b))
+        if _known_type(b):
+            return self.value.dot(_as_jax_array_(b))
+        else:
+            return b.__rmatmul__(self)
 
     def fill(self, value):
         """Fill the array with a scalar value."""
@@ -1099,7 +1115,7 @@ class EventArray(object):
 
     def sinh(
         self, *, out: Optional[Union['EventArray', ArrayLike]] = None
-    ) -> Optional[u.Quantity, jax.Array]:
+    ) -> Union[u.Quantity, jax.Array]:
         r = u.math.sinh(self.value)
         if out is None:
             return r