5b63ee72113fb0a48cc97a43e2613428ea2f7d98
Chaoming <adaduo@outlook.com>
2026-03-08 17:21:11 +0800

    feat: update performance metrics in 20_COBA_2005.py with new timing and firing rate data

--- benchmark/20_COBA_2005.py
+++ benchmark/20_COBA_2005.py
@@ -126,19 +126,19 @@ for s in [1, 2, 4, 6, 8, 10, 20, 40, 60, 80, 100]:
 # scale=100, size=400000, time = 5.40840220451355 s, firing rate = 59.569679260253906 Hz
 
 # --------------------
-# 2026/03/08, NVIDIA GeForce RTX 3080 Ti Laptop, brainevent 0.0.7, CUDA, jax 0.9.0, Ubuntu 24.04
+# 2026/03/08, NVIDIA GeForce RTX 3080 Ti Laptop, brainevent 0.0.7, CUDA 13, jax 0.9.0, Ubuntu 24.04
 #
-# scale=1, size=4000, time = 1.4490950107574463 s, firing rate = 59.573604583740234 Hz
-# scale=2, size=8000, time = -0.3793013095855713 s, firing rate = 59.56932830810547 Hz
-# scale=4, size=16000, time = 1.6740055084228516 s, firing rate = 59.568016052246094 Hz
-# scale=6, size=24000, time = 1.543215036392212 s, firing rate = 59.5692138671875 Hz
-# scale=8, size=32000, time = 1.5829763412475586 s, firing rate = 59.568077087402344 Hz
-# scale=10, size=40000, time = 1.6613562107086182 s, firing rate = 59.57008361816406 Hz
-# scale=20, size=80000, time = 1.9681501388549805 s, firing rate = 59.569087982177734 Hz
-# scale=40, size=160000, time = 3.0623559951782227 s, firing rate = 59.57011795043945 Hz
-# scale=60, size=240000, time = 4.380564212799072 s, firing rate = 59.56999969482422 Hz
-# scale=80, size=320000, time = 5.683120965957642 s, firing rate = 59.5700569152832 Hz
-# scale=100, size=400000, time = 7.005706310272217 s, firing rate = 59.569969177246094 Hz
+# scale=1, size=4000, time = 1.3123691082000732 s, firing rate = 59.57392883300781 Hz
+# scale=2, size=8000, time = 1.4384987354278564 s, firing rate = 59.57027816772461 Hz
+# scale=4, size=16000, time = 1.5230646133422852 s, firing rate = 59.569297790527344 Hz
+# scale=6, size=24000, time = 1.6051039695739746 s, firing rate = 59.56822967529297 Hz
+# scale=8, size=32000, time = 1.53263258934021 s, firing rate = 59.57051086425781 Hz
+# scale=10, size=40000, time = 1.6150670051574707 s, firing rate = 59.57093811035156 Hz
+# scale=20, size=80000, time = 2.0971567630767822 s, firing rate = 59.56829833984375 Hz
+# scale=40, size=160000, time = 2.7911770343780518 s, firing rate = 59.57014465332031 Hz
+# scale=60, size=240000, time = 4.094450235366821 s, firing rate = 59.57121276855469 Hz
+# scale=80, size=320000, time = 5.091517448425293 s, firing rate = 59.57069396972656 Hz
+# scale=100, size=400000, time = 6.501585245132446 s, firing rate = 59.56977462768555 Hz
 
 # ------------------
 # AMD Ryzen 7 7840HS
